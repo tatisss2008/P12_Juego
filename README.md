45d7d934d48c6bc7b27d7966528669e89481250e
AnaM <test>
2023-05-31 08:19:19 -0500

    V Inicial

---
+++ README.md
@@ -0,0 +1,16 @@
+//Crear la documentacion del proyecto.
+
+    <!--   Comandos GIT  11/04/2023, 12/04/2023 y 13/04/2023 -->
+
+    git init
+    git status
+    git add index.html
+    git rm --cached index.html
+    git commit
+    git clone 
+    git branch
+    git checkout
+
+
+    --- Juego Adivinar Palabra ---
+
---
+++ program.js
@@ -0,0 +1,43 @@
+
+class Teclado{
+
+    generarTeclado(){
+        const miTeclado=document.getElementById("teclado");
+
+        for (let i=65; i<=90; i++){
+            const letra = String.fromCharCode(i)
+
+             this.crearBoton(letra, miTeclado);
+
+            if (i==78){
+                this.crearBoton("Ñ", miTeclado);
+            }
+        }
+    }
+
+    crearBoton(letra, contenedor){
+        console.log(letra)
+        const btn= document.createElement("button");
+        btn.id=letra;
+        btn.textContent=letra;
+
+        btn.classList.add("btn","btn-primary","m-3")
+
+        btn.addEventListener(
+            "click",
+            ()=>{
+                this.presionarBoton(letra);
+            }
+        );
+        
+        contenedor.appendChild(btn);
+    }
+
+    presionarBoton(){
+
+    }
+
+}
+
+const  teclado= new Teclado();
+teclado.generarTeclado();
